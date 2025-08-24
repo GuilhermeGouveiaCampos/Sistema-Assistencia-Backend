d8cbcd679b66e3497dfbbe355d947e33eef31c66
Guilherme Gouveia Campos <guilherme.g.campos@academico.unirv.edu.br>
2025-08-24 18:30:27 -0300

    fix: ajuste na conexão com DB

--- db.js
+++ db.js
@@ -2,13 +2,14 @@ const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
-  password: process.env.DB_PASS || '',
+  password: process.env.DB_PASSWORD, // 👈 aqui
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
+  connectTimeout: 15000
 });
 
 module.exports = pool;
